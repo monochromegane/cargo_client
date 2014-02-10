f2216ded49bbacae4ed7e03d076b110982f8ff84
monochromegane <dev.kuro.obi@gmail.com>
2014-02-10 19:55:37 +0900

    Used gcfg package.

---
+++ cargo/cargo.go
@@ -0,0 +1,4 @@
+package cargo
+
+type Config struct {
+}
--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"code.google.com/p/gcfg"
 	"fmt"
+	"github.com/monochromegane/cargo_client/cargo"
+        "os"
 )
 
 func main() {
-	fmt.Printf("It works.\n")
+        var cfg cargo.Config
+        err := gcfg.ReadFileInto(&cfg, "Cargofile")
+        if err != nil {
+                fmt.Printf("Cargofile not found.")
+                os.Exit(1)
+        }
 }
