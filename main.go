60f8fc71e1ff51d41bc3447c3d320e9b39f6f6de
monochromegane <dev.kuro.obi@gmail.com>
2014-02-11 21:10:14 +0900

    Formated.

--- main.go
+++ main.go
@@ -5,15 +5,15 @@ import (
 	"fmt"
 	"github.com/monochromegane/cargo_client/cargo"
 	"github.com/monochromegane/cargo_client/cargo/config"
-        "os"
+	"os"
 )
 
 func main() {
-        var cfg config.Config
-        err := gcfg.ReadFileInto(&cfg, "Cargofile")
-        if err != nil {
-                fmt.Printf("Cargofile not found.\n")
-                os.Exit(1)
-        }
-        cargo.Run(cfg)
+	var cfg config.Config
+	err := gcfg.ReadFileInto(&cfg, "Cargofile")
+	if err != nil {
+		fmt.Printf("Cargofile not found.\n")
+		os.Exit(1)
+	}
+	cargo.Run(cfg)
 }
