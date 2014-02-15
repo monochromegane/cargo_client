7be60cbfb2a05621a19cbed482c30345fb585131
monochromegane <dev.kuro.obi@gmail.com>
2014-02-15 17:05:32 +0900

    Changed cargofile parse error message.

--- main.go
+++ main.go
@@ -9,14 +9,14 @@ import (
 )
 
 func main() {
-        cfg := config.DefaultConfig()
+	cfg := config.DefaultConfig()
 	err := gcfg.ReadFileInto(cfg, "Cargofile")
 	if err != nil {
-		fmt.Printf("Cargofile not found.\n")
+		fmt.Printf("Cargofile parse error.\n %s\n", err)
 		os.Exit(1)
 	}
-        cargo := cargo.Cargo{cfg}
+	cargo := cargo.Cargo{cfg}
 	cargo.SendAssets()
-        result, err := cargo.Run()
+	result, err := cargo.Run()
 	fmt.Printf("%s, %s\n", result, err)
 }
