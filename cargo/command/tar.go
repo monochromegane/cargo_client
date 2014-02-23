94a87b094ac04398cce1e277ebd83c877520383b
monochromegane <dev.kuro.obi@gmail.com>
2014-02-24 00:18:51 +0900

    Added a extract tar command to command package.

--- cargo/command/tar.go
+++ cargo/command/tar.go
@@ -7,6 +7,14 @@ import (
 type CreateTarCommand struct {
 }
 
+type ExtractTarCommand struct {
+	DestDir string
+}
+
 func (self *CreateTarCommand) Command() *exec.Cmd {
 	return exec.Command("tar", []string{"-c", "-T", "-", "--null"}...)
+}
+
+func (self *ExtractTarCommand) Command() *exec.Cmd {
+	return exec.Command("tar", []string{"x", "-C", self.DestDir}...)
 }
