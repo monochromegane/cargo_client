5e29200051c2b8b4e46a3c0591aa89c814e0bc00
monochromegane <dev.kuro.obi@gmail.com>
2014-02-24 00:20:24 +0900

    Changed implementation of pipe, because old pipe method dosen't work on exec.Cmd.

--- cargo/command/pipe.go
+++ cargo/command/pipe.go
@@ -1,37 +1,48 @@
 package command
 
 import (
+	"bytes"
 	"os/exec"
 )
 
-type pipe struct {
-	commands []*exec.Cmd
-}
+func Pipeline(cmds ...*exec.Cmd) (pipeLineOutput, collectedStandardError []byte, pipeLineError error) {
+	// Require at least one command
+	if len(cmds) < 1 {
+		return nil, nil, nil
+	}
 
-func NewPipe() *pipe {
-	return &pipe{}
-}
+	// Collect the output from the command(s)
+	var output bytes.Buffer
+	var stderr bytes.Buffer
 
-func (self *pipe) Pipe(command *exec.Cmd) *pipe {
-	self.commands = append(self.commands, command)
-	return self
-}
-
-func (self *pipe) Command() *exec.Cmd {
-	var args []string
-	var path string
-	for i, cmd := range self.commands {
-		if i == 0 {
-			path = cmd.Path
-		} else {
-			args = append(args, cmd.Path)
+	last := len(cmds) - 1
+	for i, cmd := range cmds[:last] {
+		var err error
+		// Connect each command's stdin to the previous command's stdout
+		if cmds[i+1].Stdin, err = cmd.StdoutPipe(); err != nil {
+			return nil, nil, err
 		}
-		args = append(args, cmd.Args[1:]...)
-		if i != len(self.commands)-1 {
-			args = append(args, "|")
+		// Connect each command's stderr to a buffer
+		cmd.Stderr = &stderr
+	}
+
+	// Connect the output and error for the last command
+	cmds[last].Stdout, cmds[last].Stderr = &output, &stderr
+
+	// Start each command
+	for _, cmd := range cmds {
+		if err := cmd.Start(); err != nil {
+			return output.Bytes(), stderr.Bytes(), err
 		}
 	}
-	return exec.Command(path, args...)
-}
 
-// pipe(git).pipe(tar).pipe(ssh).Command().Run()
+	// Wait for each command to complete
+	for _, cmd := range cmds {
+		if err := cmd.Wait(); err != nil {
+			return output.Bytes(), stderr.Bytes(), err
+		}
+	}
+
+	// Return the pipeline output and the collected standard error
+	return output.Bytes(), stderr.Bytes(), nil
+}
